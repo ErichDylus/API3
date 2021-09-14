ac7bf3579d26110643b751abf46e10ea65940d37
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-09-14 17:52:01 -0400

    Update SignDelegationPolicy.sol

--- governance/SignDelegationPolicy.sol
+++ governance/SignDelegationPolicy.sol
@@ -1,43 +1,43 @@
 // SPDX-License-Identifier: MIT
-// IN PROCESS, FOR DEMONSTRATION ONLY, unaudited, not recommended to be used for any purpose, carries absolutely no warranty of any kind
+// FOR DEMONSTRATION ONLY, unaudited, not recommended to be used for any purpose, carries absolutely no warranty of any kind
 // @dev sign an API3 Token Delegation and Voting Policy, with reference to their own Delegation Disclosure
 // only if address has staked API3 in API3 governance contract
 
 pragma solidity ^0.8.6;
 
-// Delegator deploys and signs their Token Delegation and Voting Policy and signs
-// this interface allows a delegate to indicate their assent by EthSign
+// Delegator deploys contract with link to disclosure and signs their Token Delegation and Voting Policy by EthSign
 
 interface VoteDelegateDisclosure {
     function sign(string calldata details) external; 
 }
 
+interface API3Pool {
+    function userSharesAt(address userAddress, uint256 _block) external view returns (uint256);
+}
+
 contract SignDelegationPolicy {
 
-    address owner;
-    address constant API3governance = 0x6dd655f10d4b9E242aE186D9050B68F725c76d76; // API3 governance staking pool
-    string public disclosureLink;
-    VoteDelegateDisclosure public policyContract;
-    mapping(address => string) disclosureHash;
+    address constant API3governance = 0x6dd655f10d4b9E242aE186D9050B68F725c76d76; // API3 governance staking pool contract address
+    VoteDelegateDisclosure public ipolicyContract;
+    API3Pool public iAPI3Pool;
     
-    event DelegateDisclosure(address indexed delegate, string signature, string IPFSlink);
+    event DelegateDisclosure(address indexed delegate, string signature, string disclosureLink);
     
-    // deployer sets token address and address of Token Delegation and Voting Policy (https://github.com/LeXpunK-Army/Token-Delegation-And-Voting-Policy)
+    // deployer sets address of Token Delegation and Voting Policy (https://github.com/LeXpunK-Army/Token-Delegation-And-Voting-Policy)
     constructor(address _policyContract) { 
-        policyContract = VoteDelegateDisclosure(_policyContract);
-        owner = msg.sender;
+        ipolicyContract = VoteDelegateDisclosure(_policyContract);
+        iAPI3Pool = API3Pool(API3governance);
     }
     
     
     function signPolicy(string memory _disclosureLink, string calldata _signature) external {
-        //require(hasStaked, "Signatory isn't an API3 DAO governor.");
-        disclosureHash[msg.sender] = _disclosureLink;
-        policyContract.sign(_signature);
+        require(iAPI3Pool.userSharesAt(msg.sender, block.number) > 0, "Signatory isn't staked in the API3 DAO governance contract.");
+        ipolicyContract.sign(_signature);
         emit DelegateDisclosure(msg.sender, _signature, _disclosureLink);
     }
     
     //check msg.sender has staked in API3 governance
-    function checkStake() external {
+    function checkStake() external view returns (uint256) {
+        return iAPI3Pool.userSharesAt(msg.sender, block.number);
     }
-
 }
