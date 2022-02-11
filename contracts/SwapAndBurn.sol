c2fad3275a90caa6d3cbe48a96061e644925be47
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2022-02-11 17:36:38 -0500

    Update SwapAndBurn.sol

--- contracts/SwapAndBurn.sol
+++ contracts/SwapAndBurn.sol
@@ -1,36 +1,47 @@
 //SPDX-License-Identifier: MIT
 // IN PROCESS AND INCOMPLETE, DO NOT USE FOR ANY PURPOSE
 // unaudited, provided without warranty of any kind, and subject to all disclosures, licenses, and caveats of this repo
-// https://github.com/sushiswap/sushiswap/blob/canary/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol https://soliditydeveloper.com/sushi-swap 
+// https://soliditydeveloper.com/sushi-swap 
 
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.0;
 
-/// @title Swap and Burn API3
+/// @title Sushiswap and Burn API3
 /// @author Varia LLC
-/// @notice uses Sushiswap/Uniswap router to swap incoming ETH for API3 tokens, then burns the API3 tokens
+/// @notice uses Sushiswap router to swap incoming ETH for API3 tokens, then burns the API3 tokens
+
+import "https://github.com/sushiswap/sushiswap/blob/canary/contracts/uniswapv2/interfaces/IUniswapV2Router02.sol";
 
 interface IERC20 { 
     function approve(address spender, uint256 amount) external returns (bool); 
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
 contract SushiswapAndBurnAPI3 {
 
-    address constant API3Token = 0x0b38210ea11411557c13457D4dA7dC6ea731B88a; // API3 token address
-    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // Wrapper Ether token address 
-    address constant SushiRouter = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F; // Sushiswap router contract address
+    address private API3Token = 0x0b38210ea11411557c13457D4dA7dC6ea731B88a; // API3 token address
+    address internal constant SUSHI_ROUTER_ADDR = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F; // Sushiswap router contract address
+
+    IUniswapV2Router02 public sushiRouter;
 
     error NoETHSent();
 
     constructor() payable {
+        sushiRouter = IUniswapV2Router02(SUSHI_ROUTER_ADDR);
         IERC20(API3Token).approve(address(this),1e24);
-        IERC20(WETH).approve(address(this),1e24);
     }
 
     function receiveAndSwap() public payable {
         if (msg.value == 0) revert NoETHSent();
-        SushiRouter.swapExactETHForTokens(msg.value,0,[WETH,API3Token],address(this),block.timestamp+10000);
+        sushiRouter.swapExactETHForTokens{ value: msg.value }(0, getPathForETHtoAPI3(), address(this), block.timestamp+100);
     }
+    
+    function getPathForETHtoAPI3() private view returns (address[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = sushiRouter.WETH();
+        path[1] = API3Token;
+        return path;
+    }
+
+    receive() payable external {}
 }
