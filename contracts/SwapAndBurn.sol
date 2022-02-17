0b20430c7c4548541d1e7ce77408541d08c5c1ca
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2022-02-17 12:20:41 -0500

    Update SwapAndBurn.sol

--- contracts/SwapAndBurn.sol
+++ contracts/SwapAndBurn.sol
@@ -4,8 +4,7 @@
 
 pragma solidity ^0.8.0;
 
-/// @title Sushiswap and Burn API3
-/// @author Varia LLC
+/// @title Swap and Burn API3
 /// @notice uses Sushiswap router to swap incoming ETH for API3 tokens, then burns the API3 tokens
 
 interface IUniswapV2Router02 {
@@ -14,10 +13,8 @@ interface IUniswapV2Router02 {
     function WETH() external pure returns (address);
 }
 
-interface IERC20 { 
-    function approve(address spender, uint256 amount) external returns (bool); 
+interface IAPI3 { 
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
     function updateBurnerStatus(bool burnerStatus) external;
     function burn(uint256 amount) external;
 }
@@ -28,16 +25,15 @@ contract SwapAndBurnAPI3 {
     address constant SUSHI_ROUTER_ADDR = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F; // Sushiswap router contract address
 
     IUniswapV2Router02 public sushiRouter;
-    IERC20 public iAPI3Token;
+    IAPI3 public iAPI3Token;
 
     error NoETHSent();
     error NoAPI3();
 
     constructor() payable {
         sushiRouter = IUniswapV2Router02(SUSHI_ROUTER_ADDR);
-        iAPI3Token = IERC20(API3_TOKEN_ADDR);
-        //iAPI3Token.approve(address(this),1e24);
-        //iAPI3Token.updateBurnerStatus(true);
+        iAPI3Token = IAPI3(API3_TOKEN_ADDR);
+        iAPI3Token.updateBurnerStatus(true);
     }
 
     function receiveAndSwap() public payable {
@@ -54,7 +50,7 @@ contract SwapAndBurnAPI3 {
     /// @return the router path for ETH/API3 swap for the receiveAndSwap() function
     function _getPathForETHtoAPI3() internal view returns (address[] memory) {
         address[] memory path = new address[](2);
-        path[0] = sushiRouter.WETH();
+        path[0] = sushiRouter.WETH(); //0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
         path[1] = API3_TOKEN_ADDR;
         return path;
     }
