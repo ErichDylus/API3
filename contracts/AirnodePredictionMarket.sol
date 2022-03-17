894c5fddfeb2fcbf935c58262c98e69767a9a80f
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2022-03-17 12:49:18 -0400

    Update AirnodePredictionMarket.sol

--- contracts/AirnodePredictionMarket.sol
+++ contracts/AirnodePredictionMarket.sol
@@ -8,41 +8,30 @@ pragma solidity >=0.8.9;
 
 /// IN PROCESS AND INCOMPLETE, unaudited and for demonstration only, subject to all disclosures, licenses, and caveats of the open-source-law repo
 /// @title Airnode Prediction Market
-// Prediction market judged by an Airnode RRP call, loosely based on https://gist.github.com/0xfoobar/c7f620e62339b0e7d201cb64f5042eef
-import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequester.sol";
+/// Prediction market judged by an Airnode RRP call, loosely based on https://gist.github.com/0xfoobar/c7f620e62339b0e7d201cb64f5042eef
+/// TODO: support ETH/native gas tokens in addition to ERC20 tokens
+
+import "https://github.com/api3dao/airnode/blob/master/packages/airnode-protocol/contracts/rrp/requesters/RrpRequester.sol";
 
 interface IERC20 { 
-    function approve(address spender, uint256 amount) external returns (bool); 
-    function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
 }
 
 contract AirnodePredictionMarket is RrpRequester {
     
-  address escrowAddress;
   address partyA;
-  address partyB;
   uint256 expiryTime;
   uint256 nextPredictionId;
-  bool isExpired;
-  bool isClosed;
-  IERC20 public ierc20;
-  string description;
-  mapping(address => bool) public parties; //map whether an address is a party to the transaction for restricted() modifier 
   mapping(bytes32 => bool) public incomingFulfillments;
   mapping(bytes32 => int256) public fulfilledData;
-  mapping(uint256 => Prediction) public predictions;
+  mapping(uint256 /* predictionId */ => bool) outcome;
+  mapping(uint256 /* predictionId */ => Prediction) public predictionList;
 
   enum Status {
         Offered,
         Accepted,
         Settled
-    }
-  enum Outcome {
-        A,
-        B,
-        Neutral
     }
 
   struct Prediction {
@@ -51,125 +40,105 @@ contract AirnodePredictionMarket is RrpRequester {
         address partyA;
         address partyB;
         address airnodeResolver;
+        string description;
         IERC20 token;
         Status status;
     }
   
-  event DealExpired(bool isExpired);
+  event Expired();
   event DealClosed(bool isClosed, uint256 effectiveTime); //event provides exact blockstamp Unix time of closing and oracle information
   event PredictionOffer(uint256 predictId);
 
-  error BuyerAddress();
-  error Expired();
-  error NotApproved();
-  error OnlyBuyer();
+  error HasExpired();
+  error NotAccepted();
+  error NotAirnodeResolver();
+  error NotOffered();
+  error NotOfferee();
+  error NotOfferor();
   error OracleConditionNotSatisfied();
+  error SameAddress();
   
-  modifier restricted() { 
-    require(parties[msg.sender], "Only parties[]");
-    _;
-  }
-  
-  /// @notice deployer (buyer) initiates escrow with description, deposit amount in USD, address of stablecoin, seconds until expiry, and designate recipient seller
-  /// @param _description should be a brief identifier of the deal in question - perhaps as to parties/underlying asset/documentation reference/hash 
-  /// @param _deposit is the purchase price which will be deposited in the smart escrow contract
-  /// @param _seller is the seller's address, who will receive the purchase price if the deal closes
-  /// @param _stablecoin is the token contract address for the stablecoin to be sent as deposit
-  /// @param _secsUntilExpiry is the number of seconds until the deal expires, which can be converted to days for front end input or the code can be adapted accordingly
-  /// @param _airnodeRrp is the public address of the AirnodeRrp.sol protocol contract on the relevant blockchain used for this contract; see: https://docs.api3.org/airnode/v0.2/reference/airnode-addresses.html
-  constructor(string memory _description, uint256 _secsUntilExpiry, address _partyB, address _stablecoin, address _airnodeRrp) RrpRequester(_airnodeRrp) {
-      if (_partyB == msg.sender) revert BuyerAddress();
+  /// @notice deployer initiates with description of prediction market, seconds until expiry, counterparty, and proper AirnodeRRP protocol contract address 
+  /// @param _secsUntilExpiry is the number of seconds until expiry of the proposed prediction market 
+  /// @param _airnodeRrpAddr is the public address of the AirnodeRrp.sol protocol contract on the relevant blockchain used for this contract; see: https://docs.api3.org/airnode/v0.2/reference/airnode-addresses.html
+  constructor(uint256 _secsUntilExpiry, address _airnodeRrpAddr) RrpRequester(_airnodeRrpAddr) {
       partyA = address(msg.sender);
-      escrowAddress = address(this);
-      ierc20 = IERC20(_stablecoin);
-      description = _description;
-      partyB = _partyB;
-      parties[msg.sender] = true;
-      parties[_partyB] = true;
-      parties[escrowAddress] = true;
       expiryTime = block.timestamp + _secsUntilExpiry;
   }
   
-    function offerPrediction(address _partyB, address _airnodeResolver, IERC20 token, uint256 amount) external {
-        IERC20(token).transferFrom(msg.sender, escrowAddress, amount); // OFFEROR MUST FIRST SEPARATELY APPROVE ESCROWADDRESS FOR AMOUNT
+    /// @param _partyB  is the counterparty's address for this offer
+    /// @param _airnodeResolver is the address of the airnode that will provide the boolean response to resolve the prediction market
+    /// @param _description should be a brief identifier or reference to terms of prediction market/agreement
+    /// @param _token is the contract address of the token in which the offer is denominated
+    /// @param _amount number of tokens for this offer, which partyB will need to match
+    function offerPrediction(string memory _description, address _partyB, address _airnodeResolver, IERC20 _token, uint256 _amount) external {
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount); // OFFEROR MUST FIRST SEPARATELY APPROVE address(this) FOR AMOUNT
         Prediction memory prediction = Prediction({
             id: nextPredictionId,
-            amount: amount,
+            amount: _amount,
             partyA: msg.sender,
             partyB: _partyB,
             airnodeResolver: _airnodeResolver,
-            token: token,
+            description: _description,
+            token: _token,
             status: Status.Offered
         });
 
-        predictions[nextPredictionId] = prediction;
-        emit PredictionOffered(nextPredictionId);
-        unchecked { nextPredictionId += 1; }
+        predictionList[nextPredictionId] = prediction;
+        emit PredictionOffer(nextPredictionId);
+	    unchecked { nextPredictionId += 1; } // will not overflow on human timelines
     }
 
-    function withdrawOffer(uint256 predictionId) external {
-        Prediction memory prediction = predictions[predictionId];
-        require(msg.sender == prediction.partyA, "Not the offer creator");
-        require(prediction.status == Status.Offered, "Not offered");
+    /// @param _predictionId prediction offer number
+    function withdrawOffer(uint256 _predictionId) external {
+        Prediction memory prediction = predictionList[_predictionId];
+        if (msg.sender != prediction.partyA) revert NotOfferor();
+        if (prediction.status != Status.Offered) revert NotOffered();
         IERC20 _token = prediction.token;
         uint256 _amount = prediction.amount;
-        delete predictions[predictionId];
+        delete predictionList[_predictionId];
         _token.transfer(msg.sender, _amount); // refund partyA
     }
 
-    function acceptPrediction(uint predictionId) external {
-        Prediction memory prediction = predictions[predictionId];
-        require(msg.sender == prediction.partyB, "Not the offer recipient");
-        require(prediction.status == Status.Offered, "Not offered");
-
+    /// @param _predictionId prediction offer number
+    function acceptPrediction(uint256 _predictionId) external {
+        Prediction memory prediction = predictionList[_predictionId];
+        if (msg.sender != prediction.partyB) revert NotOfferee();
+        if (prediction.status != Status.Offered) revert NotOffered();
         prediction.token.transferFrom(msg.sender, address(this), prediction.amount);
-        predictions[predictionId].status = Status.Accepted;
+        predictionList[_predictionId].status = Status.Accepted;
     }
 
-    function settlePrediction(uint predictionId, Outcome outcome) external {
-        Prediction memory prediction = predictions[predictionId];
-        require(msg.sender == prediction.airnodeResolver, "Not the airnodeResolver");
-        require(prediction.status == Status.Accepted, "Not accepted");
-
-        if (outcome == Outcome.A) {
-            prediction.token.transfer(prediction.partyA, 2 * prediction.amount);
-        } else if (outcome == Outcome.B) {
-            prediction.token.transfer(prediction.playerB, 2 * prediction.amount);
-        } else if (outcome == Outcome.Neutral) {
-            prediction.token.transfer(prediction.playerA, prediction.amount);
-            prediction.token.transfer(prediction.playerB, prediction.amount);
+    /// @dev if not returning a boolean response from airnode, parameters for outcome[] can be designated around fulfilledData[requestId]
+    /// @param _predictionId prediction offer number
+    function settlePrediction(uint256 _predictionId) external {
+        Prediction memory prediction = predictionList[_predictionId];
+        if (msg.sender != prediction.airnodeResolver) revert NotAirnodeResolver();
+        if (prediction.status != Status.Accepted) revert NotAccepted();
+        if (expiryTime <= block.timestamp) { 
+            IERC20(prediction.token).transfer(prediction.partyA, prediction.amount);
+            IERC20(prediction.token).transfer(prediction.partyB, prediction.amount);
         }
-
-        predictions[predictionId].status = Status.Settled;
-    }
-  
-  /// @notice check if expired, and if so, return balance to buyer 
-  function checkIfExpired() external returns(bool) {
-        if (expiryTime <= block.timestamp) {
-            isExpired = true;
-            _returnDeposit(); 
-            emit DealExpired(true);
-        } else {
-            isExpired = false;
+        // call airnode here for outcome[] -- currently hardcoded for partyA to predict true outcome, partyB to predict false
+        if (outcome[_predictionId]) {
+            IERC20(prediction.token).transfer(prediction.partyA, 2 * prediction.amount);
+        } else if (!outcome[_predictionId]) {
+            IERC20(prediction.token).transfer(prediction.partyB, 2 * prediction.amount);
         }
-        return(isExpired);
+        predictionList[_predictionId].status = Status.Settled;
     }
-    
-  /// @notice for seller to check if deposit is in escrowAddress
-  function checkBalance() external restricted view returns(uint256) {
-      return ierc20.balanceOf(escrowAddress);
-  }
   
   /// @notice call the airnode which will provide a boolean response
   /// @dev inbound API parameters which may already be ABI encoded. Source: https://docs.api3.org/airnode/v0.2/grp-developers/call-an-airnode.html
-  /// @param airnode the address of the relevant API provider's airnode
+  /// @param _predictionId prediction offer number
   /// @param endpointId identifier for the specific endpoint desired to access via the airnode
   /// @param sponsor address of the entity that pays for the fulfillment of a request & gas costs the Airnode will incur. These costs will be withdrawn from the sponsorWallet of the Airnode when the requester calls it.
   /// @param sponsorWallet the wallet created via mnemonic by the sponsor with the Admin CLI, funds within used by the airnode to pay gas. See https://docs.api3.org/airnode/v0.2/grp-developers/requesters-sponsors.html#what-is-a-sponsor
   /// @param parameters specify the API and reserved parameters (see Airnode ABI specifications at https://docs.api3.org/airnode/v0.2/reference/specifications/airnode-abi-specifications.html for how these are encoded)
-  function callAirnode(address airnode, bytes32 endpointId, address sponsor, address sponsorWallet, bytes calldata parameters) external {
-      bytes32 requestId = airnodeRrp.makeFullRequest( // Make the Airnode request
-          airnode,                        
+  function callAirnode(uint256 _predictionId, bytes32 endpointId, address sponsor, address sponsorWallet, bytes calldata parameters) external {
+	Prediction memory prediction = predictionList[_predictionId];      
+	bytes32 requestId = airnodeRrp.makeFullRequest( // Make the Airnode request
+          prediction.airnodeResolver,                        
           endpointId,                     
           sponsor,                        
           sponsorWallet,                  
@@ -188,6 +157,5 @@ contract AirnodePredictionMarket is RrpRequester {
       delete incomingFulfillments[requestId];
       int256 _decodedData = abi.decode(data, (int256));
       fulfilledData[requestId] = _decodedData;
-      _closeDeal(_decodedData);
   }
 }
