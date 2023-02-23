dd8463508f58a0d66347ac5f5d190d7d2237f4c5
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2023-02-23 15:57:00 -0500

    updates and optimizations

--- contracts/AirnodeEthEscrow.sol
+++ contracts/AirnodeEthEscrow.sol
@@ -9,20 +9,20 @@ pragma solidity >=0.8.9;
 /// @title Airnode ETH Escrow
 /// @notice bilateral smart escrow contract, with ETH as payment, expiration denominated in seconds, deposit refunded if contract expires before closeDeal() called, contingent on valid Airnode response as parameterized in _closeDeal()
 /// TODO: allow a partial deposit
-import "https://github.com/api3dao/airnode/blob/master/packages/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+import {RrpRequesterV0} from "https://github.com/api3dao/airnode/blob/master/packages/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
 
 contract AirnodeEthEscrow is RrpRequesterV0 {
-    address payable buyer;
-    address payable seller;
-    uint256 deposit;
-    uint256 expiryTime;
-    bool sellerApproved;
-    bool buyerApproved;
+    address payable public immutable buyer;
+    address payable public seller;
+    uint256 public immutable deposit;
+    uint256 public immutable expiryTime;
+    bool public sellerApproved;
+    bool public buyerApproved;
     bool isExpired;
     bool isClosed;
-    bool internal lock;
+
     string description;
-    mapping(address => bool) public parties; //map whether an address is a party to the transaction for restricted() modifier
+
     mapping(bytes32 => bool) public incomingFulfillments;
     mapping(bytes32 => int256) public fulfilledData;
 
@@ -32,22 +32,12 @@ contract AirnodeEthEscrow is RrpRequesterV0 {
     error BuyerAddress();
     error DepositAmount();
     error Expired();
+    error NoRequest();
     error NotApproved();
     error OnlyBuyer();
+    error OnlyBuyerOrSeller();
     error OracleConditionNotSatisfied();
     error PaymentFailed();
-
-    modifier restricted() {
-        require(parties[msg.sender], "Only parties[]");
-        _;
-    }
-
-    modifier nonReentrant() {
-        require(!lock, "No re-entering");
-        lock = true;
-        _;
-        lock = false;
-    }
 
     /// @notice deployer (buyer) initiates escrow with description, deposit amount in USD, address of stablecoin, seconds until expiry, and designate recipient seller
     /// @param _description should be a brief identifier of the deal in question - perhaps as to parties/underlying asset/documentation reference/hash
@@ -67,41 +57,26 @@ contract AirnodeEthEscrow is RrpRequesterV0 {
         deposit = _deposit;
         description = _description;
         seller = payable(_seller);
-        parties[msg.sender] = true;
-        parties[_seller] = true;
-        parties[address(this)] = true;
         expiryTime = block.timestamp + _secsUntilExpiry;
     }
 
     /// @notice buyer may confirm seller's recipient address as extra security measure or change seller address
     /// @param _seller is the new recipient address of seller
-    function designateSeller(address payable _seller) external restricted {
+    function designateSeller(address payable _seller) external {
         if (_seller == buyer) revert BuyerAddress();
+        if (msg.sender != buyer || msg.sender != seller)
+            revert OnlyBuyerOrSeller();
         if (isExpired) revert Expired();
-        parties[_seller] = true;
         seller = _seller;
     }
 
     /// @notice buyer deposits in this contract, requires exact wei deposit amount
-    function depositInEscrow() external payable nonReentrant returns (bool, uint256) {
+    function depositInEscrow() external payable returns (bool, uint256) {
         if (msg.sender != buyer) revert OnlyBuyer();
-        if (msg.value != deposit) revert DepositAmount(); // requires buyer deposit exactly the deposit amount in wei
+        if (msg.value != deposit) revert DepositAmount();
         (bool sent, ) = address(this).call{value: deposit}("");
         if (!sent) revert PaymentFailed();
-        return (true, address(this).balance);
-    }
 
-    /// @notice this contract returns deposit to buyer
-    function _returnDeposit() private nonReentrant returns (bool, uint256) {
-        (bool sent, ) = buyer.call{value: deposit}("");
-        if (!sent) revert PaymentFailed();
-        return (true, address(this).balance);
-    }
-
-    /// @notice this contract sends deposit to seller
-    function _paySeller() private nonReentrant returns (bool, uint256) {
-        (bool sent, ) = seller.call{value: deposit}("");
-        if (!sent) revert PaymentFailed();
         return (true, address(this).balance);
     }
 
@@ -118,21 +93,19 @@ contract AirnodeEthEscrow is RrpRequesterV0 {
     }
 
     /// @notice for seller to check if deposit is in escrow
-    function checkEscrow() external view restricted returns (uint256) {
+    function checkEscrow() external view returns (uint256) {
         return address(this).balance;
     }
 
     /// if buyer wishes to initiate dispute over seller breach of off chain agreement or repudiate, simply may wait for expiration without sending deposit nor calling this function
-    function readyToClose() external restricted returns (string memory) {
+    /// @notice for 'buyer' and 'seller' to each call this method when ready to close, in order for _closeDeal() to execute
+    function readyToClose() external returns (bool, bool) {
         if (msg.sender == seller) {
             sellerApproved = true;
-            return ("Seller ready to close.");
         } else if (msg.sender == buyer) {
             buyerApproved = true;
-            return ("Buyer ready to close.");
-        } else {
-            return ("Neither buyer nor seller.");
         }
+        return (sellerApproved, buyerApproved);
     }
 
     /// @notice call the applicable airnode when ready to close
@@ -170,7 +143,7 @@ contract AirnodeEthEscrow is RrpRequesterV0 {
         external
         onlyAirnodeRrp
     {
-        require(incomingFulfillments[requestId], "No Request");
+        if (!incomingFulfillments[requestId]) revert NoRequest();
         delete incomingFulfillments[requestId];
         int256 _decodedData = abi.decode(data, (int256));
         fulfilledData[requestId] = _decodedData;
@@ -180,11 +153,7 @@ contract AirnodeEthEscrow is RrpRequesterV0 {
     /// @notice checks if both buyer and seller are ready to close and expiration has not been met; if so, closes deal and pays seller; if not, deposit returned to buyer
     /// @dev if properly closes, emits event with effective time of closing. This function is private to prevent external submission of valid _decodedData to trigger closing.
     /// @param _decodedData airnode response passed by fulfill()
-    function _closeDeal(int256 _decodedData)
-        private
-        nonReentrant
-        returns (bool)
-    {
+    function _closeDeal(int256 _decodedData) internal returns (bool) {
         if (_decodedData <= 0) revert OracleConditionNotSatisfied(); //change this condition for applicable triggering data/params/range etc. from Airnode
         if (expiryTime <= block.timestamp) {
             isExpired = true;
@@ -192,9 +161,17 @@ contract AirnodeEthEscrow is RrpRequesterV0 {
             emit DealExpired(block.timestamp);
         } else {
             isClosed = true;
-            _paySeller();
+            (bool sent, ) = seller.call{value: deposit}("");
+            if (!sent) revert PaymentFailed();
             emit DealClosed(true, block.timestamp); // confirmation of deal closing and effective time upon payment to seller
         }
         return (isClosed);
+    }
+
+    /// @notice this contract returns deposit to buyer
+    function _returnDeposit() internal returns (bool, uint256) {
+        (bool sent, ) = buyer.call{value: deposit}("");
+        if (!sent) revert PaymentFailed();
+        return (true, address(this).balance);
     }
 }
